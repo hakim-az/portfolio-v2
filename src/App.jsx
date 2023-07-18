a84f300ee3292703886662876a935bffc2701d45
azzaz abdelhakim <hakimazzaz93@gmail.com>
2023-07-18 08:14:00 +0100

    Portfolio v2 starter files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='w-full h-screen text-white bg-gray-900 text-center pt-96 text-4xl'>PORTFOLIO STARTER</h1>
+    </>
+  )
+}
+
+export default App
