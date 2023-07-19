5d77cfe9dfc1d245624bf25210ff35e3ba35aefb
azzaz abdelhakim <hakimazzaz93@gmail.com>
2023-07-19 10:30:38 +0100

    ADD the Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import About from './components/About/About'
 import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 import Projects from './components/Projects/Projects'
@@ -16,6 +17,7 @@ function App() {
       <About/>
       <Projects/>
       <Contact/>
+      <Footer/>
     </>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="bg-gray-900 py-16">
+                <h3 className="text-white text-lg text-center">Azzaz abdelhakim &#169; 2023</h3>
+        </footer>
+    )
+}
+
+export default Footer
