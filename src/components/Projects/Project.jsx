670926b0bb9f0d6657bc22bba224b475a3687c97
azzaz abdelhakim <hakimazzaz93@gmail.com>
2023-07-20 11:42:19 +0100

    UPDATE the Projects, Project components - add the projects array of objects for each project and implementing the logic for view more/less buttons and adding the assets/projects file

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,18 +1,24 @@
 import Website from '../../assets/images/website.png'
+import Figma from '../../assets/images/white-figma.svg'
 import GitHub from '../../assets/skills/github.svg'
 
 
-const Project = () => {
+const Project = ({image, title, githubUrl, viewUrl, figmaUrl}) => {
   return (
     <div className='w-2/5 mb-10' style={{minWidth: '350px'}}>
-        <img className='w-full h-80 bg-white rounded max-[834px]:h-56' src="#" alt="." />
+        <div className='w-full h-80 bg-white rounded max-[834px]:h-56' style={{backgroundImage : `url(${image})`, backgroundPosition : 'center', backgroundSize: 'cover', backgroundRepeat : 'no-repeat'}}>
+            
+        </div>
         <div className='mt-3 flex items-center justify-between'>
-            <h3 className='text-white text-xl font-medium'>Project Title</h3>
+            <h3 className='text-white text-xl font-medium'> {title} </h3>
             <div className='flex gap-3'>
-                <a href="#">
+                <a href={figmaUrl} target="_blank">
+                    <img className=' w-7 h-7' src={Figma} alt="figma-icon" />
+                </a>
+                <a href={githubUrl} target="_blank">
                     <img className=' w-7 h-7' src={GitHub} alt="github-icon" />
                 </a>
-                <a href="#">
+                <a href={viewUrl} target="_blank">
                     <img className=' w-7 h-7' src={Website} alt="website-icon" />
                 </a>
             </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,24 +1,131 @@
+import { useState } from 'react'
 import Title from '../Title/Title'
 import Project from './Project'
+import ProjectOne from '../../assets/projects/project-1.png'
+import ProjectTwo from '../../assets/projects/project-2.png'
+import ProjectThree from '../../assets/projects/project-3.png'
+import ProjectFour from '../../assets/projects/project-4.png'
+import ProjectFive from '../../assets/projects/project-5.png'
+import ProjectSix from '../../assets/projects/project-6.png'
+import ProjectSeven from '../../assets/projects/project-7.png'
+import ProjectHeight from '../../assets/projects/project-8.png'
+import ProjectNine from '../../assets/projects/project-9.png'
+import ProjectTen from '../../assets/projects/project-10.png'
 
 
 const Projects = () => {
+
+  const projects = [
+    {
+      id : 1,
+      image : ProjectOne,
+      title : 'Life Time',
+      viewUrl : 'https://life-time.netlify.app/',
+      githubUrl : '#',
+      figmaUrl : '#',
+    },
+    {
+      id : 2,
+      image : ProjectTwo,
+      title : 'Sunny',
+      viewUrl : 'https://comforting-licorice-482ab1.netlify.app/',
+      githubUrl : '#',
+      figmaUrl : '#',
+    },
+    {
+      id : 3,
+      image : ProjectThree,
+      title : 'Furni Shop',
+      viewUrl : 'https://furni-shop-dz.netlify.app/',
+      githubUrl : '#',
+      figmaUrl : '#',
+    },
+    {
+      id : 4,
+      image : ProjectFour,
+      title : 'Agency Exconsultancy',
+      viewUrl : 'https://agency-exconsultancy.netlify.app/',
+      githubUrl : '#',
+      figmaUrl : '#',
+    },
+    {
+      id : 5,
+      image : ProjectFive,
+      title : 'GPT-3 Website',
+      viewUrl : 'https://gpt-3-website.netlify.app/',
+      githubUrl : '#',
+      figmaUrl : '#',
+    },
+    {
+      id : 6,
+      image : ProjectSix,
+      title : 'Easy Bank',
+      viewUrl : 'https://web-05-easybank.netlify.app/',
+      githubUrl : '#',
+      figmaUrl : '#',
+    },
+    {
+      id : 7,
+      image : ProjectSeven,
+      title : 'Blogr',
+      viewUrl : 'https://web-03-blogr.netlify.app/',
+      githubUrl : '#',
+      figmaUrl : '#',
+    },
+    {
+      id : 8,
+      image : ProjectHeight,
+      title : 'Loop Studios',
+      viewUrl : 'https://web-04-loopstudios.netlify.app/',
+      githubUrl : '#',
+      figmaUrl : '#',
+    },
+    {
+      id : 9,
+      image : ProjectNine,
+      title : 'Focal Portfolio',
+      viewUrl : 'https://web-01-foacl-portfolio.netlify.app/',
+      githubUrl : '#',
+      figmaUrl : '#',
+    },
+    {
+      id : 10,
+      image : ProjectTen,
+      title : 'Sunny Side',
+      viewUrl : 'https://web-02-sunnyside.netlify.app/',
+      githubUrl : '#',
+      figmaUrl : '#',
+    }
+  ];
+
+  const [more, setMore] = useState(6);
+  const [view, setView] = useState(true);
+
+  const handleMore = () => {
+    setMore(projects.length)
+    setView(false)
+  }
+  const handleLess = () => {
+    setMore(6)
+    setView(true)
+  }
 
   return (
     <section id="projects" className='bg-gray-900 pb-20'>
         <div className="box-container flex items-center justify-evenly flex-wrap">
             <Title Title='Projetcs'/>
             {/* Projects */}
-            <Project />
-            <Project />
-            <Project />
-            <Project />
-            <Project />
-            <Project />
-            {/* Button view more */}
-            <div className='w-full flex items-center justify-center mt-4'>
-                <button className='px-5 py-2 bg-green-600 rounded-sm text-base text-white hover:bg-green-500'>View more</button>
-            </div>
+            {projects.slice(0,more).map((project) => <Project key={project.id} image={project.image} title={project.title} viewUrl={project.viewUrl} githubUrl={project.githubUrl} figmaUrl={project.figmaUrl} /> )}
+            {/* Button view more/less */}
+            {
+              view 
+              ? <div className='w-full flex items-center justify-center mt-4' >
+                  <button className='px-5 py-2 bg-green-600 rounded-sm text-base text-white hover:bg-green-500' onClick={handleMore}>View more</button>
+                </div>
+              : <div className='w-full flex items-center justify-center mt-4' >
+                  <button className='px-5 py-2 bg-green-600 rounded-sm text-base text-white hover:bg-green-500' onClick={handleLess}>View less</button>
+                </div> 
+            }
         </div>
     </section>
   )
